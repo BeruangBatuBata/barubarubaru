b3f696dd6ad9b36fd47d27fd881c7a51030b9349
BeruangBatuBata <92040313+BeruangBatuBata@users.noreply.github.com>
2025-09-26 15:21:25 +0700

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -22,7 +22,7 @@ def build_sidebar():
             f"""
             <div style="display: flex;align-items: center; gap: 0px; margin-bottom: 20px;">
                 <img src="data:image/png;base64,{logo_base64}" style="width: 40px; height: 60px;margin-right: 0px;">
-                <div style="font-size: 1.4em; font-weight: bold; color: #fafafa; line-height: 1.3;">
+                <div style="font-size: 2em; font-weight: bold; color: #fafafa; line-height: 1.3;">
                     MLBB Pro-scene<br>
                     Analytics Dashboard
                 </div>
