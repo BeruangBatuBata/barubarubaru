f1c0274305fa706c4a8d4621e48d05cc446b7bff
BeruangBatuBata <92040313+BeruangBatuBata@users.noreply.github.com>
2025-09-26 15:23:56 +0700

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -20,7 +20,7 @@ def build_sidebar():
     if logo_base64:
         st.sidebar.markdown(
             f"""
-            <div style="display: flex;align-items: center; gap: 0px; margin-bottom: 20px;">
+            <div style="display: flex;align-items: center;justify-content: center; gap: 0px; margin-bottom: 20px;">
                 <img src="data:image/png;base64,{logo_base64}" style="width: 40px; height: 60px;margin-right: 0px;">
                 <div style="font-size: 1.5em; font-weight: bold; color: #fafafa; line-height: 1.3;">
                     MLBB Pro-scene<br>
